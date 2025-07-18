77a8d8209d4b887ce366af71f90fb4c5ef9b5536
mahmoudAmagdy <58373205+mahmoudAmagdy@users.noreply.github.com>
2025-07-18 07:34:17 +0300

    adding the backend and frontend
    adding backend project (Java Spring Boot) and the frontend project (React)

---
+++ backend/src/main/java/com/mam/vm/VendingMachineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mam.vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mam/vm/VendingMachineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mam.vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
